61581839c85fa97edc0b2f8d288d201d68cf8fb6
Kevin McGuinness <kevin.mcguinness@gmail.com>
2020-05-29 22:27:27 +0100

    Stuff for PyPi

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import sys
+
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
 
 setup(
    name='pdf2pptx',
    version='1.0',
    description='Utility to convert a PDF slideshow to Powerpoint PPTX.',
+   long_description=long_description,
+   long_description_content_type='text/markdown',
    author='Kevin McGuinness',
+   url='https://github.com/kevinmcguinness/pdf2pptx',
+   license='MIT',
    author_email='kevin.mcguinness@gmail.com',
    packages=[], 
    install_requires=['pymupdf', 'python-pptx'],
    entry_points={
        'console_scripts': ['pdf2pptx=pdf2pptx:main'],
-   }
+   },
+   classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Topic :: Multimedia :: Graphics :: Presentation',
+    ],
 )
