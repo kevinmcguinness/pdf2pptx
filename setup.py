c04c6c9e79e0f0898c8ad8664be441b3e0c4a2e8
Kevin McGuinness <kevin.mcguinness@gmail.com>
2020-05-29 23:25:13 +0100

    fix missing dep

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
    name='pdf2pptx',
-   version='1.0.2',
+   version='1.0.3',
    description='Utility to convert a PDF slideshow to Powerpoint PPTX.',
    long_description=long_description,
    long_description_content_type='text/markdown',
@@ -21,7 +21,7 @@ setup(
    license='MIT',
    author_email='kevin.mcguinness@gmail.com',
    packages=['pdf2pptx'],
-   install_requires=['pymupdf', 'python-pptx', 'click'],
+   install_requires=['pymupdf', 'python-pptx', 'click', 'tqdm'],
    entry_points={
        'console_scripts': ['pdf2pptx=pdf2pptx.cli:main'],
    },
